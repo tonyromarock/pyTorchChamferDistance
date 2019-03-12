73c5cd61e0d9673f1e01170ec8a43a908b2cd941
Peter Mortimer <pjmortimer95@googlemail.com>
2019-03-12 10:53:36 +0100

    Added chamfer example more like FoldingNet

--- example.py
+++ example.py
@@ -3,15 +3,25 @@ import torch
 
 from chamfer_distance import ChamferDistance
 chamfer_dist = ChamferDistance()
+#
+# sample usage as external in factored3d: 
+#   python3  factored3d/external/chamfer/example.py  
+#
+# points is a [b x n x 3] matrix
+# points_reconstructed is a [b x m x 3] matrix
+# Notice that m and n do not have to have the same size, but the 
+# pcds in the same batch need to be of the same size to make up
+# a matrix.
 
-#...
-# points and points_reconstructed are [n_points x 3] matrices
+points = torch.from_numpy(np.array([ [ [10.,0.,0.], [0.,10.,0.] ] ])).float()
+points_reconstructed = torch.from_numpy(np.array([ [ [9.,0.,0.], [0.,9.,0.], [12.,0.,0.] ] ])).float()
 
-points = torch.from_numpy(np.array([ [ [3.,0.,0.], [0.,3.,0.] ],  [ [10.,0.,0.], [0.,10.,0.] ] ])).float()
-points_reconstructed = torch.from_numpy(np.array([ [ [5.,0.,0.], [0.,5.,0.] ]  ,[ [9.,0.,0.], [0.,9.,0.] ] ])).float()
+# This loss defintion is close to what is used for FoldingNet
+# reconstruction loss. They do not square the L2 distance as 
+# we do.
 
 dist1, dist2 = chamfer_dist(points, points_reconstructed)
-loss = (torch.mean(dist1)) + (torch.mean(dist2))
+loss = max((torch.mean(dist1)),(torch.mean(dist2)))
 
 print(dist1)
 print(dist2)
